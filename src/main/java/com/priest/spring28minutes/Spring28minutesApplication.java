6defbf0c7387d35685f024b7f7e8cf5ce2a2f853
thangmv6 <vumanhthang89@gmail.com>
2018-03-11 11:15:53 +0700

    internationalization message

--- src/main/java/com/priest/spring28minutes/Spring28minutesApplication.java
+++ src/main/java/com/priest/spring28minutes/Spring28minutesApplication.java
@@ -1,12 +1,32 @@
 package com.priest.spring28minutes;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class Spring28minutesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring28minutesApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSourceResourceBundle() {
+		ResourceBundleMessageSource bundleMessageSource = new ResourceBundleMessageSource();
+		bundleMessageSource.setBasename("/messages");
+		return bundleMessageSource;
 	}
 }
--- src/main/java/com/priest/spring28minutes/controller/UserController.java
+++ src/main/java/com/priest/spring28minutes/controller/UserController.java
@@ -4,10 +4,12 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 import java.util.List;
+import java.util.Locale;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.priest.spring28minutes.dao.UserDAO;
@@ -25,6 +28,9 @@ import com.priest.spring28minutes.exception.UserException;
 public class UserController {
 	@Autowired
 	private UserDAO userDao;
+
+	@Autowired
+	private MessageSource messageSource;
 
 	@GetMapping(value = "/users/{userId}")
 	public Resource<User> findUser(@PathVariable("userId") Integer userId) {
@@ -54,6 +60,11 @@ public class UserController {
 	@PostMapping(value = "/users")
 	public User createUser(@Valid @RequestBody User user) {
 		return userDao.save(user);
+	}
+
+	@GetMapping(value = "/hello-message")
+	public String getHelloMessage(@RequestHeader(value = "Accept-Language", required = false) Locale locale) {
+		return messageSource.getMessage("message.user.hello", null, locale);
 	}
 
 }
